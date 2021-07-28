02835c9d81c3a1e9ae41d5c590a179acebce60e8
arthay <arthur15251@gmail.com>
2021-07-28 20:34:37 +0400

    add public key setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,20 +4,21 @@ set -e
 
 : ${WPE_SSHG_KEY_PRIVATE?Required secret not set.}
 
-#SSH Key Vars 
+#SSH Key Vars
 SSH_PATH="$HOME/.ssh"
 KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
 WPE_SSHG_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
+WPE_SSHG_KEY_KEY_PUBLIC_PATH="$SSH_PATH/github_action.pub"
 
 ###
-# If you'd like to expand the environments, 
+# If you'd like to expand the environments,
 # Just copy/paste an elif line and the following export
 # Then adjust variables to match the new ones you added in main.yml
 #
 # Example:
 #
 # elif [[ ${GITHUB_REP} =~ ${NEW_BRANCH_NAME}$ ]]; then
-#     export WPE_ENV_NAME=${NEW_ENV_NAME};    
+#     export WPE_ENV_NAME=${NEW_ENV_NAME};
 ###
 
 if [[ $GITHUB_REF =~ ${PRD_BRANCH}$ ]]; then
@@ -25,16 +26,16 @@ if [[ $GITHUB_REF =~ ${PRD_BRANCH}$ ]]; then
 elif [[ $GITHUB_REF =~ ${STG_BRANCH}$ ]]; then
     export WPE_ENV_NAME=$STG_ENV;
 elif [[ $GITHUB_REF =~ ${DEV_BRANCH}$ ]]; then
-    export WPE_ENV_NAME=$DEV_ENV;    
-else 
+    export WPE_ENV_NAME=$DEV_ENV;
+else
     echo "FAILURE: Branch name required." && exit 1;
 fi
 
 #Deploy Vars
 WPE_SSH_HOST="$WPE_ENV_NAME.ssh.wpengine.net"
-if [ -n "$TPO_PATH" ]; then 
+if [ -n "$TPO_PATH" ]; then
     DIR_PATH="$TPO_PATH"
-else 
+else
     DIR_PATH=""
 fi
 
@@ -55,11 +56,13 @@ ssh-keyscan -t rsa "$WPE_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
 
 #Copy Secret Keys to container
 echo "$WPE_SSHG_KEY_PRIVATE" > "$WPE_SSHG_KEY_PRIVATE_PATH"
+echo "$WPE_SSHG_SSH_KEY_PUBLIC" > "$WPE_SSHG_KEY_PUBLIC_PATH"
 
-#Set Key Perms 
+#Set Key Perms
 chmod 700 "$SSH_PATH"
 chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$WPE_SSHG_KEY_PRIVATE_PATH"
+chmod 644 "$WPE_SSHG_KEY_PUBLIC_PATH"
 
 # Lint before deploy
 if [ "${PHP_LINT^^}" == "TRUE" ]; then
@@ -72,13 +75,13 @@ if [ "${PHP_LINT^^}" == "TRUE" ]; then
         fi
     done
     echo "End PHP Linting."
-else 
+else
     echo "Skipping PHP Linting."
 fi
 
 # Deploy via SSH
 rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --inplace --out-format="%n"  --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
 
-# Clear cache 
+# Clear cache
 ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no $WPE_SSH_USER "cd sites/${WPE_ENV_NAME} && wp page-cache flush"
 echo "SUCCESS: Site has been deployed and cache has been flushed."
